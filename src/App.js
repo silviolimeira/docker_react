7fbf59b13ddad57ae15810843fe1a4759103e7bf
silviolimeira <sicemal@gmail.com>
2023-04-25 11:15:33 -0300

    add Dockerfile.prod, docker-compose dev and prod

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>{`Hello ${process.env.REACT_APP_NAME}`}</p>
-        <p>002</p>
+        <p>004</p>
         <a
           className="App-link"
           href="https://reactjs.org"
